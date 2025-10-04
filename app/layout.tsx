6f9e28f25b2c2e60517ce314999cb8156ebaa8a0
yanwe111 <77040812+yanwe111@users.noreply.github.com>
2025-10-04 11:55:14 +0700

    Move project to root directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Chúc mừng Tết Trung Thu',
+  title: 'Minh Thư dễ thương',
   description: 'Chúc mừng Tết Trung Thu',
   generator: 'Chúc mừng Tết Trung Thu',
 }
